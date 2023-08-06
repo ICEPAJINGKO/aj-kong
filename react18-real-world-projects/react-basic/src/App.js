460be5c4d787fb943513274ff2d79d9d901dbb1f
ICEPAJINGKO <icepajingko@gmail.com>
2023-08-06 14:37:59 +0700

    array state

--- react18-real-world-projects/react-basic/src/App.js
+++ react18-real-world-projects/react-basic/src/App.js
@@ -1,26 +1,28 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [students, setStudent] = useState([
+    { id: "1", name: "Ant" },
+    { id: "2", name: "Bird" },
+    { id: "3", name: "Cat" },
+    { id: "4", name: "Dog" },
+  ]);
 
-  function addCount() {
-    setCount(count + 1);
-  }
-
-  function downCount() {
-    setCount(count - 1);
-  }
-
-  function resetCount() {
-    setCount(0);
+  function deleteStudent(id){
+    setStudent(students.filter((stud) => stud.id !== id));
   }
 
   return (
     <>
-      <h1>{count}</h1>
-      <button onClick={addCount}>add</button>
-      <button onClick={downCount}>down</button>
-      <button onClick={resetCount}>reset</button>
+      <h1>มีนักเรียนทั้งหมด : {students.length}</h1>
+      <ul>
+        {students.map((stud) => (
+          <li key={stud.id}>
+            <p>{stud.id} : {stud.name}</p>
+            <button onClick={() => deleteStudent(stud.id)}>delete</button>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
